b8eb11db48ae86763191f2095c6d611c8a2b3ef1
hjlAlex <huangjielong_hjl@163.com>
2017-11-02 11:22:40 +0800

    Update dao.js

--- src/main/resources/dao.js
+++ src/main/resources/dao.js
@@ -6,7 +6,7 @@ var ioc = {
 			depose : "close"
 		},
 		fields : {
-			url : "jdbc:mysql://127.0.0.1:3306/adminex?useUnicode=true&characterEncoding=utf8&autoReconnect=true&failOverReadOnly=false",
+			url : "jdbc:mysql://192.168.1.116:3306/adminex?useUnicode=true&characterEncoding=utf8&autoReconnect=true&failOverReadOnly=false",
 			username : "root",
 			password : "",
 			testWhileIdle : true,
